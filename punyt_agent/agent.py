177567b2b178fdf25f7b9c2d79bf67805b13b63b
DreamyDolphin <130183370+DreamyDolphin@users.noreply.github.com>
2025-05-16 19:54:51 +0530

    Delete punyt_agent directory

--- punyt_agent/agent.py
+++
@@ -1,9 +0,0 @@
-from google.adk.agents import Agent
-
-
-root_agent = Agent(
-    name="punyt_agent",
-    model="gemini-2.0-flash",
-    description="'punyt' agent",
-    instruction="You are a 'punyt' agent. you always talk in hinglish. In each and every response, replace every 'u', 'i' and 'e' with a 'y' and then give the final response, do this only while you use a hinglish word"
-)
